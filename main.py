20063cf13f82a75b338945a55c96d5986237233b
Samuel <113396050+Samuelz38@users.noreply.github.com>
2024-11-15 13:52:42 -0300

    1.4
    atualização de arquivos e páginas. Adição da Página de Pesquisa

--- main.py
+++ main.py
@@ -1,40 +1,59 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLayout
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
 from PySide6.QtGui import QIcon
-from sistema_recomendacao.view.descricao import Ui_Form
-from sistema_recomendacao.view.mainwindow import Ui_MainWindow
+from view.descricao import Ui_Form
+from view.mainwindow import Ui_MainWindow
+from view.pagina_pesquisa import Ui_PesquisaPagina
 from PySide6.QtCore import Qt
 import sys
 import os
 
 
-class MainWindow(QMainWindow,Ui_MainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super(MainWindow,self).__init__()
+        super(MainWindow, self).__init__()
         self.setupUi(self)
         self.setWindowTitle('Sistema de Recomendação')
         self.setWindowFlag(Qt.WindowMaximizeButtonHint, False)
-        self.intem_lista = self.lista_listWidget
-        self.intem_lista.itemClicked.connect(self.open_system)
 
-    def open_system(self):
-            self.w = DescricaoIntem()
-            self.w.show()
-            
-            
+        self.enviar_pushButton.clicked.connect(self.abrir_pagina_pesquisa)
 
-class DescricaoIntem(QWidget,Ui_Form):
+    def abrir_pagina_pesquisa(self):
+        self.pesquisa_pagina = PaginaPesquisa()
+        self.pesquisa_pagina.show()
+        self.close()
+
+
+class PaginaPesquisa(QWidget, Ui_PesquisaPagina):
     def __init__(self):
-        super(DescricaoIntem,self).__init__()
+        super(PaginaPesquisa, self).__init__()
+        self.setupUi(self)
+        self.setWindowTitle('Página de Pesquisa')
+        self.setWindowFlag(Qt.WindowMaximizeButtonHint, False)
+       
+        self.lista_listWidget.itemClicked.connect(self.abrir_pagina_descricao)
+        self.voltar_pushButton.clicked.connect(self.abrir_pagina_main)
+
+    def abrir_pagina_descricao(self):
+        self.pagina_desscricao = DescricaoIntem()
+        self.pagina_desscricao.show()
+    
+    def abrir_pagina_main(self):
+        self.pagina_desscricao = MainWindow()
+        self.pagina_desscricao.show()
+        self.close()
+
+
+class DescricaoIntem(QWidget, Ui_Form):
+    def __init__(self):
+        super(DescricaoIntem, self).__init__()
         self.setupUi(self)
         self.setWindowTitle('Descrição')
-
-    
-    pass
+        self.setWindowFlag(Qt.WindowMaximizeButtonHint, False)       
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon('sistema_recomendacao/view/assets/icons8-controle-64.png'))
+    app.setWindowIcon(QIcon('view/assets/icons8-controle-64.png'))
     window = MainWindow()
     window.show()
     app.exec()
